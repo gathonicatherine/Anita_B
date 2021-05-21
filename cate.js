104b4d6a2c5f303e36e077e8d8c0646a0223458c
gathonicatherine <catherrinewanjiku@gmail.com>
2021-05-21 21:04:09 +0000

    project

--- cate.js
+++ cate.js
@@ -22,13 +22,13 @@
 //               }
 //     console.log(myFavouriteFruit)
 
-// function add (a=100,b=2){
-//       console.log(a+b);
-//       }
-//       (a,b)=>returna+100;
-//       (a,b)=>a+100;
-      // let sum = (a,b)=>a+b;
-      //Arrow function 
+function add (a=100,b=2){
+      console.log(a+b);
+      }
+      (a,b)=>returna+100;
+      (a,b)=>a+100;
+      let sum = (a,b)=>a+b;
+      
 
       let sum = (a,b)=>a+b
       console.log(sum(5,2));
@@ -53,23 +53,23 @@
      };
      Car.other ("Kenya")        
 
-let item={
-      item1:"Orange",
-      item2:"Mango",
-      item3:"Avocado",
-      item4:"Peach",
-      cost:function(orangePrice,mangoPrice,avocadoPrice,peachPrice){
-            return`I am buying 3 ${item.item1} at ${orangePrice*2},6 ${item.item2} at ${mangoPrice*6} ,2 ${this.item4} at ${peachPrice*2} and ${item.item3} at ${avocadoPrice}`
-      },
+// let item={
+//       item1:"Orange",
+//       item2:"Mango",
+//       item3:"Avocado",
+//       item4:"Peach",
+//       cost:function(orangePrice,mangoPrice,avocadoPrice,peachPrice){
+//             return`I am buying 3 ${item.item1} at ${orangePrice*2},6 ${item.item2} at ${mangoPrice*6} ,2 ${this.item4} at ${peachPrice*2} and ${item.item3} at ${avocadoPrice}`
+//       },
 
-console.log(item.cost(20,35,120,50)); 
+// console.log(item.cost(20,35,120,50));
 
 
-function favNumber(x, callback){
-      console.log( x + 'is my favourite number');   
-      console.log(`${x} is my favourite number`);  
-     callback();
-}
+// function favNumber(x, callback){
+//       console.log( x + 'is my favourite number');   
+//       console.log(`${x} is my favourite number`);  
+//      callback();
+// }
 function otherNumber(){
       console.log(456);
 
